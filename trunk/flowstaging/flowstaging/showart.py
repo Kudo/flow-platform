ea3995f099178868a8df5320c1e920eb409fde7e
wangpy.tw <wangpy.tw@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2008-12-08 07:23:26 +0000

    * restructured UI template layout

--- trunk/flowstaging/flowstaging/showart.py
+++ trunk/flowstaging/flowstaging/showart.py
@@ -2,24 +2,54 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 showArtPages = [ 
-                {'filename'    : 'register.html', 
-                 'name'        : 'Register', 
-                 'description' : 'Register page',
+                {'filename'    : 'base.html', 
+                 'name'        : 'Base Page', 
+                 'description' : 'The base of HTML page',
+                 'owner'       : 'brian_wang', 
+                 'status'      : 'DONE'},
+                {'filename'    : 'splashbox.html', 
+                 'name'        : 'Splash Box', 
+                 'description' : 'included by Base Page',
+                 'owner'       : 'brian_wang', 
+                 'status'      : 'DONE'},
+                {'filename'    : 'register_base.html', 
+                 'name'        : 'Register: Base Page', 
+                 'description' : 'extends Base Page',
+                 'owner'       : 'brian_wang', 
+                 'status'      : 'DONE'},
+                {'filename'    : 'register_new_account.html', 
+                 'name'        : 'Register: New Account', 
+                 'description' : 'new account page, extends Register Base Page',
                  'owner'       : 'brian_wang', 
                  'status'      : 'TODO'},
-                {'filename'    : 'profile.html', 
-                 'name'        : 'Profile',
-                 'description' : 'Profile page', 
+                {'filename'    : 'twocolumn_base.html', 
+                 'name'        : 'Two Column: Base Page', 
+                 'description' : 'two column layout template, extends Base Page',
+                 'owner'       : 'tom_chen', 
+                 'status'      : 'DONE'},
+                {'filename'    : 'profile_base.html', 
+                 'name'        : 'Profile: Base Page', 
+                 'description' : 'extends Two Column Base Page',
+                 'owner'       : 'tom_chen', 
+                 'status'      : 'DONE'},
+                {'filename'    : 'profile_leftcolumn.html', 
+                 'name'        : 'Profile: Sidebar', 
+                 'description' : 'sidebar content, included by Profile Base Page',
                  'owner'       : 'tom_chen', 
                  'status'      : 'TODO'},
+                {'filename'    : 'profile_content.html', 
+                 'name'        : 'Profile: Sample Content',
+                 'description' : 'sample content, extends Profile Base Page', 
+                 'owner'       : 'tom_chen', 
+                 'status'      : 'DONE'},
                 ]
 
 def showartAction(request, filename):
     if ('' == filename):
         return render_to_response('showart.html', {'showart_pages' : showArtPages})
     else:
-        try:
-            response = render_to_response(filename, {})
-        except:
-            response = HttpResponse('Page not found: '+filename)
+        #try:
+        response = render_to_response(filename, {})
+        #except:
+        #    response = HttpResponse('Page not found: '+filename)
         return response
