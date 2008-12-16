eabcc3c674e7ee58d1bdc8b9710cc0128062af72
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2008-12-16 14:18:47 +0000

    Running local server will support static files.

--- trunk/flowstaging/flowstaging/urls.py
+++ trunk/flowstaging/flowstaging/urls.py
@@ -3,11 +3,13 @@ from django.conf.urls.defaults import *
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Example:
     # (r'^flowstaging/', include('flowstaging.foo.urls')),
-
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}),
     # showart pages
     (r'^showart/(?P<filename>.*)$', 'showart.showartAction'),
     # temp
