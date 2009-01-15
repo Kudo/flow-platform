229b4f5ddbee68070e750f84b8b4806067e32420
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-01-15 14:40:45 +0000

    merged event team milestone I finished

--- trunk/flow1.0/src/flow-site/eventView/eventList.py
+++ trunk/flow1.0/src/flow-site/eventView/eventList.py
@@ -1,30 +1,29 @@
+import datetime
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-#from django.template import TemplateDoesNotExists
-import datetime
 from google.appengine.ext import db
 from db import ddl3
 
 def mainPage(request): 
-        
+    if request.path=='/':
+        return HttpResponseRedirect('event/')
+    
     now = datetime.datetime.now()
     event1 = ddl3.EventProfile.gql('where status in (:1,:2)','approved','recruiting').fetch(5)
     event2 = ddl3.EventProfile.gql('where status in (:1,:2)','approved','recruiting').fetch(5)
     event3 = ddl3.EventProfile.gql('where status in (:1)','activity closed').fetch(5)
-    #recruitings = db.GqlQuery("SELECT * FROM RecruitingSpec")
     listActivityResult1 = []
     listActivityResult2 = []
     listActivityResult3 = []
     dictRecruitingResult = {}
-    #for recruiting in recruitings:
-     #   dictRecruitingResult[recruiting.activity_id]=recruiting.head_count_required
       
     for event in event1:   
         if now < event.end_time:
             listActivityResult1.append(
              {'event_id':event.event_id,
              'event_name':event.event_name,
-             'originator':event.originator,
+             'originator':event.npo_profile_ref.npo_name,
              'create_time':event.create_time.strftime('%Y-%m-%d %H:%M'),
              'start_time':event.start_time.strftime('%Y-%m-%d %H:%M'),
              'event_region':u','.join(event.event_region),
@@ -39,21 +38,20 @@ def mainPage(request):
         listActivityResult2.append(
             {'event_id':event.event_id,
              'event_name':event.event_name,
-             'originator':event.originator,
+             'originator':event.npo_profile_ref.npo_name,
              'create_time':event.create_time.strftime('%Y-%m-%d %H:%M'),
              'start_time':event.start_time.strftime('%Y-%m-%d %H:%M'),
              'event_region':u','.join(event.event_region),
              'description':event.description,
              'lackpeople':intLackPeople,
              'event_key':str(event.key()),
-             #'headcount':dictRecruitingResult[event.event_id]
              }
              )
     for event in event3:   
         listActivityResult3.append(
          {'event_id':event.event_id,
          'event_name':event.event_name,
-         'originator':event.originator,
+         'originator':event.npo_profile_ref.npo_name,
          'create_time':event.create_time.strftime('%Y-%m-%d %H:%M'),
          'start_time':event.start_time.strftime('%Y-%m-%d %H:%M'),
          'event_region':u','.join(event.event_region),
--- trunk/flow1.0/src/flow-site/npoView/addEvent.py
+++ trunk/flow1.0/src/flow-site/npoView/addEvent.py
@@ -1,42 +1,46 @@
-# -*- coding: UTF-8 -*-
-import time
-from django.http import HttpResponse
+import time, re
+from datetime import datetime
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django.template import TemplateDoesNotExist
-from db import ddl3
 from google.appengine.ext import db
-from datetime import datetime
 from django import forms
+from db import ddl3
 
 class NewEventForm(forms.Form):
-    txtActivityName = forms.CharField(label=u"活動名稱*",required=True,widget=forms.TextInput(attrs={'size':'37'}))
-    txtStartDate = forms.DateTimeField(label=u"開始時間*",required=True,initial='2009-01-14',widget=forms.DateTimeInput(attrs={'size':'20'}))
-    txtEndDate = forms.DateTimeField(label=u"結束時間*",required=True,initial='2009-01-14',widget=forms.DateTimeInput(attrs={'size':'20'}))
-    txtPlace = forms.CharField(label=u"地點*",required=True,initial='Taipei',widget=forms.TextInput(attrs={'size':'59'}))
-    txtVolunteerHour = forms.IntegerField(label=u"志工服務*",required=True,min_value=0,initial=1,widget=forms.TextInput(attrs={'size':'20'}))
-    txtServiceTarget = forms.CharField(label=u"服務對象*",required=True,initial='B',widget=forms.TextInput(attrs={'size':'38'}))
-    txtLabel = forms.CharField(label=u"標籤(關鍵字)*",required=True,initial='C',widget=forms.TextInput(attrs={'size':'20'}))
-    txtOriginator = forms.CharField(label=u"發起人",required=False,widget=forms.TextInput(attrs={'size':'20'}))
-    txtOrignatorGroup = forms.CharField(label=u"發起群組",required=False,widget=forms.TextInput(attrs={'size':'20'}))
-    txtMobile0 = forms.CharField(label=u"連絡手機*",required=True,initial='0900000000',widget=forms.TextInput(attrs={'size':'21'}))
-    txtActivityTarget = forms.CharField(label=u"活動目的*",required=True,initial='D',widget=forms.TextInput(attrs={'size':'49'}))
-    txtActivityContent = forms.CharField(label=u"活動內容*",required=False,widget=forms.Textarea())
-    txtActivityCost = forms.IntegerField(label=u"費用金額",required=False,widget=forms.TextInput(attrs={'size':'20'}))
-    txtRegistrationFee = forms.IntegerField(label=u"報名費 ",required=False,widget=forms.TextInput(attrs={'size':'20'}))
-    txtProjectProposalAddress = forms.URLField(label=u"活動企劃書位址*",required=True,initial='http://www.google.com.tw',widget=forms.TextInput(attrs={'size':'58'}))
+    txtActivityName = forms.CharField(required=True,widget=forms.TextInput(attrs={'size':'37'}))
+    description = forms.CharField(required=True,initial=u'event description',widget=forms.Textarea(attrs={'rows':'4', 'cols':'40'}))
+    txtStartDate = forms.DateTimeField(required=True,initial=datetime.utcnow(),widget=forms.DateTimeInput(attrs={'size':'20'}))
+    txtEndDate = forms.DateTimeField(required=True,initial=datetime.utcfromtimestamp(time.time()+86400),widget=forms.DateTimeInput(attrs={'size':'20'}))
+    txtPlace = forms.CharField(required=True,initial='Taipei',widget=forms.TextInput(attrs={'size':'59'}))
+    txtVolunteerHour = forms.IntegerField(required=True,min_value=0,initial=1,widget=forms.TextInput(attrs={'size':'20'}))
+    txtServiceTarget = forms.CharField(required=True,initial='B',widget=forms.TextInput(attrs={'size':'38'}))
+    txtLabel = forms.CharField(required=True,initial='C',widget=forms.TextInput(attrs={'size':'20'}))
+    txtOriginator = forms.CharField(required=False,widget=forms.TextInput(attrs={'size':'20'}))
+    txtOrignatorGroup = forms.CharField(required=False,widget=forms.TextInput(attrs={'size':'20'}))
+    txtMobile0 = forms.CharField(required=True,initial='0900000000',widget=forms.TextInput(attrs={'size':'21'}))
+    txtActivityTarget = forms.CharField(required=True,initial='D',widget=forms.TextInput(attrs={'size':'49'}))
+    txtActivityContent = forms.CharField(required=True,initial=u'Content',widget=forms.Textarea())
+    txtActivityCost = forms.IntegerField(required=False,widget=forms.TextInput(attrs={'size':'20'}))
+    txtRegistrationFee = forms.IntegerField(required=False,widget=forms.TextInput(attrs={'size':'20'}))
+    txtProjectProposalAddress = forms.URLField(required=True,initial='http://www.google.com.tw',widget=forms.TextInput(attrs={'size':'58'}))
+
+def splitData(strData,strToken=',|;| '):
+    return [s.strip() for s in re.split(strToken,strData) if s.strip()]
 
  # save event into database
 def processAddEvent(request):
               
     # Fetch event input from request
-    if request.method == 'POST':
+    if request.method != 'POST':
+        return HttpResponseRedirect('listEvent')
         
-        submitType = request.POST['submitType']
+    submitType = request.POST['submitType']
     
-        if('cancel' == submitType):
-            return HttpResponseRedirect('listEvent')              # Because there is no activity_admin page now
-        
+    if(submitType=='cancel'):
+        return HttpResponseRedirect('listEvent')
+    if(submitType==''):
+        form = NewEventForm()
+    else:
         form = NewEventForm(request.POST)
         if form.is_valid():
         # Create New Activity Instance                       
@@ -44,13 +48,11 @@ def processAddEvent(request):
             npoRef = ddl3.NpoProfile.all().get()
             questionnaire_template_ref = ddl3.QuestionnaireTemplate.all().get()
             volunteer_profile_ref = ddl3.VolunteerProfile.all().get()
-            tagValue = form.cleaned_data['txtLabel']
-            tagList = [tagValue]           
-            
+
             newEventDict = {            
             'event_name' : form.cleaned_data['txtActivityName'],
             'event_id' : str(int(time.time())),       # must 10 digital value
-            'description' : 'UI not perform!!',
+            'description' : form.cleaned_data['description'],
             'npo_profile_ref' : npoRef,
             'volunteer_profile_ref' : volunteer_profile_ref,
             'category' : 'TBD',
@@ -67,10 +69,10 @@ def processAddEvent(request):
             'npo_event_rating' : 10,
             'create_time' : datetime.now(),
             'update_time' : datetime.now(),
-            'event_region': [form.cleaned_data['txtPlace']],
+            'event_region': splitData(form.cleaned_data['txtPlace']),
             'event_hours' : form.cleaned_data['txtVolunteerHour'],
             'event_target' : [form.cleaned_data['txtServiceTarget']],
-            'tag' : tagList,
+            'tag' : splitData(form.cleaned_data['txtLabel']),
             'summary' : form.cleaned_data['txtActivityContent'],
             'attachment_links' : [db.Link(form.cleaned_data['txtProjectProposalAddress'])],
             'expense' : form.cleaned_data['txtActivityCost'],
@@ -89,19 +91,20 @@ def processAddEvent(request):
             if('' == newEventDict.get('summary')):
                 newEventDict['summary']=None
             
-            newEventProfile = ddl3.EventProfile(**newEventDict)            
+            newEventProfile = ddl3.EventProfile(**newEventDict)
         
         
         # Save into datastore based on submit type
             if('send' == submitType):
-                newEventProfile.status = 'approved'            
+                newEventProfile.status = 'approved'
+                newEventProfile.approved=True
+                newEventProfile.approved_time=datetime.now()
             
                         
             # Save into database
             newEventProfile.put() 
             return HttpResponseRedirect('listEvent')
-    else:
-        form = NewEventForm()   
+        
     
     return render_to_response('event/event-admin-add.html', {'form': form})
 
