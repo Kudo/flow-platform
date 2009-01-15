07bdb6c021ffad135a326aa29bdec622f2f249e0
camgelo@gmail.com <camgelo@gmail.com@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-01-15 10:14:29 +0000

    npo view handler

---
+++ flow1.0/src/flow-site/npoView/eventAdminListAction.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import TemplateDoesNotExist
+from django import forms
+from google.appengine.ext import db
+from db import ddl3
+
+
+
+def eventAction(request):
+    #print request.POST['event_id']
+    return render_to_response(r'event\event-admin-cancel.html', {})
+    
