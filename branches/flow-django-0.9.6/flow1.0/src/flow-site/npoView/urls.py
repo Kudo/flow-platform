8aad6f8bf9c37c911f5f5d4f92154de4f435b020
ckchien <ckchien@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-01-16 07:48:19 +0000

    Change to django 0.9.6

--- branches/flow-django-0.9.6/flow1.0/src/flow-site/npoView/urls.py
+++ branches/flow-django-0.9.6/flow1.0/src/flow-site/npoView/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('npoView',   
-    (r'^addEvent$','addEvent.processAddEvent'),
-    (r'^listEvent$', 'eventMgmt.mainPage'),
+    (r'^addEvent/?$',               'addEvent.processAddEvent'),
+    (r'^listEvent/?$',              'eventMgmt.mainPage'),
 )
