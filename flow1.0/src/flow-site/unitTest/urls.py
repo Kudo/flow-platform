953fef8433bb463275157e04be65c2e1e9ebbe0c
ckchien <ckchien@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-02-12 17:32:21 +0000

    1. Add /unitTest/createTest/ this link for display current test script url.
    2. Merge some code from r97, r98.
    
    3. Complete npo list in volunteer home.

--- flow1.0/src/flow-site/unitTest/urls.py
+++ flow1.0/src/flow-site/unitTest/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('unitTest',   
     (r'^volunteer/createTestData/?$',           'volunteer.create'),
     (r'^countryCity/createTestData/?$',         'country.create'),
+    (r'^createTestData/?$',                     'createTestData.showCreateTestData'),
+    (r'^createTestData/kudo/?$',                'kudo.create'),
 )
