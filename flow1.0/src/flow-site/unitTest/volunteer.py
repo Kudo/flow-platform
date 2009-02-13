b8265a13dd7e20c42a347794f77bb3dcc1909bcb
ckchien <ckchien@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-02-13 09:27:53 +0000

    Complete volunteer list

--- flow1.0/src/flow-site/unitTest/volunteer.py
+++ flow1.0/src/flow-site/unitTest/volunteer.py
@@ -13,7 +13,7 @@ def create(request):
     try:
         user = users.User("ckchien@gmail.com")
         kudo = VolunteerProfile(volunteer_id=user, id_no="M123456789", volunteer_last_name="Chien", volunteer_first_name="Kudo", gmail=user.email(),
-                                     date_birth=datetime.date(1984, 6, 2), expertise=[u"Nothing"], sex="Male", phone_no="02-1234-5678", resident_country=u"中華民國",
+                                     date_birth=datetime.date(1984, 6, 2), expertise=["Nothing"], sex="Male", phone_no="02-1234-5678", resident_country=u"中華民國",
                                      resident_postal="104", resident_state=u"台灣省", resident_city=u"台北市", resident_district=u"大安區",
                                      prefer_region=[u'台北', u'台中'], prefer_zip=[u'104'], prefer_target=[u'老弱婦孺'], prefer_field=[u'聊天'], prefer_group=[u'不拘'],
                                      create_time=now, update_time=now, volunteer_rating=80, status="normal", search_text="Kudo in the house",
@@ -59,4 +59,28 @@ def create(request):
         return response
 
     response.write('新增 VolunteerProfile 成功!!!')
+    return response
+
+def bulkCreate(request):
+    response = HttpResponse(mimetype="text/plain; charset=utf-8")
+    now       = datetime.datetime.utcnow()
+    try:
+        for i in range(0, 100):
+            user = users.User("ckchien" + str(i) + "@gmail.com")
+            volunteer = VolunteerProfile(volunteer_id=user, id_no="M123456789", volunteer_last_name="Chien", volunteer_first_name="Kudo", gmail=user.email(),
+                                         date_birth=datetime.date(1984, 6, 2), expertise=[], sex="Male", phone_no="02-1234-5678", resident_country=u"中華民國",
+                                         resident_postal="104", resident_state=u"台灣省", resident_city=u"台北市", resident_district=u"大安區",
+                                         prefer_region=[u'台北', u'台中'], prefer_zip=[u'104'], prefer_target=[u'老弱婦孺'], prefer_field=[u'聊天'], prefer_group=[u'不拘'],
+                                         create_time=now, update_time=now, volunteer_rating=80, status="normal", search_text="Kudo in the house",
+                                         valid_google_acct=True, cellphone_no='0912-345-678', tag=['Kudo', u'測試'], organization='Consumer',
+                                         blog='http://www.kudo.idv.tw/blog/', brief_intro='Nobody', logo='http://farm4.static.flickr.com/3238/3042955911_f0730a2640.jpg',
+                                         total_serv_hours=20, total_reg_events=5, total_serv_events=6,
+                                         )
+
+            volunteer.put()
+    except:
+        response.write('大量新增 VolunteerProfile 失敗 (%s)' % str(sys.exc_info()))
+        return response
+
+    response.write('大量新增 VolunteerProfile 成功!!!')
     return response
