e66eefe5038aa2f2186c05a8bcc86176e4d118f7
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-02-25 03:02:32 +0000

    use %ProgramFiles% instead of real path

--- trunk/flow1.0/src/runDevServer.bat
+++ trunk/flow1.0/src/runDevServer.bat
@@ -1,2 +1,2 @@
 set PYTHONPATH=%PYTHONPATH%;%CD%\flow-site;%PYTHONPATH%;%CD%\flow-site\lib
-python "c:\Program Files\Google\google_appengine\dev_appserver.py" flow-site %1 %2 %3 %4 
+python "%ProgramFiles%\Google\google_appengine\dev_appserver.py" flow-site %1 %2 %3 %4 
