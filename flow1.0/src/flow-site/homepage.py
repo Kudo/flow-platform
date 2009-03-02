7fc022c4886a1888244cae7dfd92cb15a06cfd87
ctchen <ctchen@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-03-02 15:04:57 +0000

    Add homepage (unfinished), and some adjustments in global.css

---
+++ flow1.0/src/flow-site/homepage.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render_to_response
+import flowBase
+
+def home(request):
+    template_values = {
+            'base':                     flowBase.getBase(request),
+    }
+    return render_to_response('homepage.html', template_values)
