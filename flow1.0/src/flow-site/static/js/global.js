867edd98f12231038461862c04d801d14a99664d
ckchien <ckchien@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-03-17 07:01:35 +0000

    Tagging the old flowstaging folder

--- flow1.0/src/flow-site/static/js/global.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    // If get image failed, load default image instead
-    $("img").error(function () {
-      $(this).unbind("error").attr("src", "/static/images/head_blue50.jpg");
-    });
-});
--- flow1.0/src/runDevServer.bat
+++
@@ -1,2 +0,0 @@
-set PYTHONPATH=%PYTHONPATH%;%CD%\flow-site;%PYTHONPATH%;%CD%\flow-site\lib
-python "%ProgramFiles%\Google\google_appengine\dev_appserver.py" flow-site %1 %2 %3 %4 
