df01967d7ec2e521679c515335b7e7dcc78e8865
ckchien <ckchien@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-03-23 03:24:47 +0000

    Update homepage.py

--- trunk/flow1.0/src/flow-site/homepage.py
+++ trunk/flow1.0/src/flow-site/homepage.py
@@ -5,6 +5,7 @@ from db.ddl import VolunteerProfile, NpoProfile, EventProfile
 import flowBase
 
 displayEventCount = 5
+displayExpertiseCount = 5
 
 def home(request):
     now = datetime.datetime.now()
@@ -12,25 +13,39 @@ def home(request):
     tmpList = VolunteerProfile.all().order('-id').fetch(10)
     count = len(tmpList)
     randomIndex = random.randint(0, count - 1)
-    volunteer = tmpList[randomIndex]
+    volunteerObj = tmpList[randomIndex]
+    volunteerObj.showExpertise = u', '.join(volunteerObj.expertise[:displayExpertiseCount])
+    if (len(volunteerObj.expertise) > displayExpertiseCount):
+        volunteerObj.showExpertise += u', ...'
+    volunteer = {
+        'name':             u', '.join([volunteerObj.volunteer_first_name, volunteerObj.volunteer_last_name]),
+        'region':           volunteerObj.resident_city,
+        'showExpertise':    volunteerObj.showExpertise,
+        'logo':             volunteerObj.logo,
+    }
 
     tmpList = NpoProfile.all().order('-id').fetch(10)
     count = len(tmpList)
     randomIndex = random.randint(0, count - 1)
-    npo = tmpList[randomIndex]
+    npoObj = tmpList[randomIndex]
+    npo = {
+        'name':             npoObj.npo_name,
+        'region':           npoObj.service_region,
+        'description':      npoObj.brief_intro,
+    }
 
-    del tmpList, count, randomIndex
+    del tmpList, count, randomIndex, volunteerObj, npoObj
 
     eventList = []
     for event in EventProfile.all().filter('status in ', ['approved', 'recruiting']).order('-create_time').fetch(displayEventCount):
         if event.start_time > now:
             eventList.append({
-                'eventKey': str(event.key()),
-                'name': event.event_name,
-                'npoName': event.npo_profile_ref.npo_name,
-                'region': u','.join(event.event_region),
-                'startTime': event.start_time.strftime('%Y-%m-%d %H:%M'),
-                'description': event.description,
+                'eventKey':     str(event.key()),
+                'name':         event.event_name,
+                'npoName':      event.npo_profile_ref.npo_name,
+                'region':       u','.join(event.event_region),
+                'startTime':    event.start_time.strftime('%Y-%m-%d %H:%M'),
+                'description':  event.description,
             })
 
     template_values = {
