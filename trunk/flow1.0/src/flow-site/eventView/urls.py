b881ca129a71d1446fab77bfa76424b24dbccfe9
ctchen <ctchen@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-03-24 15:44:41 +0000

    Add Event Full Page List (first draft)

--- trunk/flow1.0/src/flow-site/eventView/urls.py
+++ trunk/flow1.0/src/flow-site/eventView/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('eventView',
     (r'^viewEvent/?$',              'eventInfo.showEvent'),
     (r'^/?$',                       'eventList.mainPage'),
+    (r'^fullViewEvent/?$',          'eventFullPageList.mainPage'),
     (r'^applyEvent/?$',             'eventInfo.applyEvent'),
     (r'^mailToFriend/?$',           'eventInfo.mailToFriend'),
     (r'^EmptyApply/?$',             'eventInfo.EmptyApply'),
