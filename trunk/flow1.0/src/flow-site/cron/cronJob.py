a554e27466eeee5060d32a3fd61b3bcec18f1123
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-03-26 18:58:31 +0000

    1. add daily cron job test 2. test Google Mail API

--- trunk/flow1.0/src/flow-site/cron/cronJob.py
+++ trunk/flow1.0/src/flow-site/cron/cronJob.py
@@ -1,5 +1,8 @@
-import logging
+# -*- coding: big5 -*-
+import logging,datetime
 from django.http import HttpResponse
+from google.appengine.api import mail
+from db import ddl
 
 def every5minutes(request):
     logging.info('every5minutes cron test')
@@ -7,4 +10,17 @@ def every5minutes(request):
 
 def hourly(request):
     logging.info('hourly cron test')
+    return HttpResponse('ok')
+
+def daily(request):
+    logging.info('daily cron test')
+    message = mail.EmailMessage()
+    message.subject=u'若水志工媒合平台'
+    message.sender = 'camgelo@gmail.com'
+    message.to = 'chienchih_lo@trend.com.tw'
+    message.body = u"""測試GAE的cron job! %s\r\n"""%(datetime.datetime.utcnow()+datetime.timedelta(hours=8))
+    for event in ddl.EventProfile.gql('ORDER BY create_time DESC').fetch(100):
+        message.body+='%s %s %s %s\r\n'%(event.event_name, event.npo_profile_ref.npo_name, event.create_time.strftime('%Y-%m-%d %H:%M'), event.description)
+    message.send()
+    
     return HttpResponse('ok')
--- trunk/flow1.0/src/flow-site/cron/urls.py
+++ trunk/flow1.0/src/flow-site/cron/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('cron',
     (r'^5m/?$',       'cronJob.every5minutes'),
     (r'^hourly/?$',   'cronJob.hourly'),
+    (r'^daily/?$',    'cronJob.daily'),
 )
