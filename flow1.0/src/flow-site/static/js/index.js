524d541c1a471102fc6d4124ce36cb7f6c1e3746
ctchen <ctchen@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-03-30 15:59:46 +0000

    Integrate Derek's search bar and flash.

---
+++ flow1.0/src/flow-site/static/js/index.js
@@ -0,0 +1,34 @@
+﻿// JScript File
+function jsInit(){
+    document.getElementById("Search_Wrap").style.display = "none";
+}
+function jsShowSearchPanel(){
+    document.getElementById("Search_Wrap").style.display = "block";
+    jsSwitchType("Activity");
+}
+function jsCloseSearchPanel(){
+    document.getElementById("Search_Wrap").style.display = "none";
+}
+function jsSwitchType(strType){
+    document.getElementById("Type_Activity").className = "type";
+    document.getElementById("Search_Activity").style.display = "none";
+    document.getElementById("Type_Volunteer").className = "type";
+    document.getElementById("Search_Volunteer").style.display = "none";
+    document.getElementById("Type_Group").className = "type"; 
+    document.getElementById("Search_Group").style.display = "none";     
+    switch (strType)
+    { 
+        case "Activity":
+            document.getElementById("Type_Activity").className = "selected";
+            document.getElementById("Search_Activity").style.display = "block";
+            break;
+        case "Volunteer":
+            document.getElementById("Type_Volunteer").className = "selected";
+            document.getElementById("Search_Volunteer").style.display = "block";
+            break;
+        case "Group":
+            document.getElementById("Type_Group").className = "selected"; 
+        document.getElementById("Search_Group").style.display = "block";   
+            break;
+    } 
+}
