9053e8dff5db6e1c01cf9ae8cafdff9f378a3370
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-04-15 19:19:25 +0000

    Use Django i18n framework to translate English to Tradition Chinese

--- flow1.0/src/flow-site/compileI18N.bat
+++ flow1.0/src/flow-site/compileI18N.bat
@@ -1,1 +1,5 @@
+: You need to install "GetText for Windows" before run this script.
+: "GetText for Windows" site: http://gnuwin32.sourceforge.net/packages/gettext.htm
+: Installation program: http://gnuwin32.sourceforge.net/downlinks/gettext.php
+: You need to add "C:\Program Files\GnuWin32\bin" into PATH after install completed.
 python "c:\Program Files\Google\google_appengine\lib\django\django\bin\compile-messages.py"
