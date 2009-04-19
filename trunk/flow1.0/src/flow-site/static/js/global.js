881f490beec9d61270484016a0c23e83ceb79a31
ctchen <ctchen@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-04-19 12:33:41 +0000

    Fix Search Bar becoming a long list. (Issue 65)

--- trunk/flow1.0/src/flow-site/static/js/global.js
+++ trunk/flow1.0/src/flow-site/static/js/global.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // If get image failed, load default image instead
     $("img").error(function () {
-      $(this).unbind("error").attr("src", "/static/images/head_blue50.jpg");
+      $(this).unbind("error").attr("src", "/static/images/volunteer50.gif");
     });
     position_search_bar();
 });
