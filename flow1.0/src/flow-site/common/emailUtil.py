7d030f97593782c9491dd85e9f148953ac41934b
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-04-24 16:56:37 +0000

    1. send email notify to volunteer when NPO have approved her application 2. bug fix: event.approved_count is incorrect 3. check if event.volunteer_shortage==0

--- flow1.0/src/flow-site/common/emailUtil.py
+++ flow1.0/src/flow-site/common/emailUtil.py
@@ -19,6 +19,13 @@ STR_CANCEL_CONTENT=u'''
 若水志工媒合平台
 '''
 
+STR_JOIN_APPROVED_CONTENT=u'''
+
+您報名參加"%(event)s"活動，已經通過審核。
+
+若水志工媒合平台
+'''
+
 def sendEventCancelMail(objVolunteer,objEvent,strReason):
     message = mail.EmailMessage()
     message.subject=u'[若水志工媒合平台] 活動取消通知'
@@ -26,3 +33,12 @@ def sendEventCancelMail(objVolunteer,objEvent,strReason):
     message.to = objVolunteer.volunteer_id.email()
     message.body = STR_CANCEL_CONTENT%{'event':objEvent.event_name,'reason':strReason}
     message.send()
+
+def sendJoinEventApprovedMail(objVolunteer,objEvent):
+    message = mail.EmailMessage()
+    message.subject=u'[若水志工媒合平台] 您報名參加的活動已審核通過'
+    message.sender = settings.ADMIN_EMAIL
+    message.to = objVolunteer.volunteer_id.email()
+    message.body = STR_JOIN_APPROVED_CONTENT%{'event':objEvent.event_name}
+    message.send()
+    
