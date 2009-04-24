9e24f7e17d1b2bb8d14d3ed2513999c419584e8a
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-04-24 16:56:37 +0000

    1. send email notify to volunteer when NPO have approved her application 2. bug fix: event.approved_count is incorrect 3. check if event.volunteer_shortage==0

--- trunk/flow1.0/src/flow-site/npoView/eventAdminValidate.py
+++ trunk/flow1.0/src/flow-site/npoView/eventAdminValidate.py
@@ -1,5 +1,5 @@
 # -*- coding: big5 -*-
-import datetime
+import datetime,logging
 from django.http import HttpResponseRedirect,HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.template import TemplateDoesNotExist
@@ -7,6 +7,7 @@ from google.appengine.ext import db
 from db import ddl
 from django import newforms as forms
 import flowBase
+from common import emailUtil
 
 # Check to see if eventID is given. Direct to error page if not.
 def volunteerShow(request,npoid):
@@ -32,6 +33,7 @@ def volunteerShow(request,npoid):
     dicData={'lstVolunteer' : addName(result1),
              'lstApproved' : addName(result2),
              'base':flowBase.getBase(request,'npo'),
+             'npoProfile': objNpo,
              'event':event,
              'event_key':event.key(),
              'page':'event'
@@ -47,7 +49,10 @@ def fixedSizeStr(s,intSize):
 def addName(lstVolEvent):
     lstVolunteer=[]
     for volEvent in lstVolEvent:
-        objVolunteer = volEvent.volunteer_profile_ref
+        try:
+            objVolunteer = volEvent.volunteer_profile_ref
+        except:
+            continue
         objVolunteer.age = (datetime.date.today() - objVolunteer.date_birth).days / 365
         objVolunteer.dbKey = volEvent.key()
         objVolunteer.expertiseSum=fixedSizeStr(u','.join(objVolunteer.expertise),6)
@@ -68,25 +73,29 @@ def approveVolunteer(request,npoid):
         raise AssertionError("event is None")
     if event.npo_profile_ref.id!=objNpo.id:
         raise AssertionError("event.npo_profile_ref.id!=objNpo.id")
-    if 'approved' not in request.POST:
+    if 'approved' not in request.POST or event.volunteer_shortage==0:
         return HttpResponseRedirect('/npo/%s/admin/listEvent'%npoid)
     
     # Process the data in form.cleaned_data
     query = db.GqlQuery("SELECT * FROM VolunteerEvent WHERE event_profile_ref = :1 AND status = :2",event,'approved')
     lstPreApproved = [obj.volunteer_profile_ref for obj in query.fetch(1000)]
-    
+    event.approved_count=len(lstPreApproved)
+    event.volunteer_shortage=event.volunteer_req-event.approved_count
     lstApprovedVol = request.POST['approved']
-    if not isinstance(lstApprovedVol,list) or not isinstance(lstApprovedVol,tuple):
+    if not isinstance(lstApprovedVol,list) and not isinstance(lstApprovedVol,tuple):
         lstApprovedVol=[lstApprovedVol]
     for volKey in lstApprovedVol:
-        vol=db.get(db.Key(volKey))
-        if vol in lstPreApproved:
-            lstPreApproved.remove(vol)
+        objRec=db.get(db.Key(volKey))
+        if not objRec:
             continue
-        vol.status='approved'
-        vol.approved_time=datetime.datetime.utcnow()
-        vol.put()
-    event.approved_count=len(lstApprovedVol)
+        if objRec in lstPreApproved:
+            lstPreApproved.remove(objRec)
+            continue
+        objRec.status='approved'
+        objRec.approved_time=datetime.datetime.utcnow()
+        emailUtil.sendJoinEventApprovedMail(objRec.volunteer_profile_ref,event)
+        objRec.put()
+        event.approved_count+=1
     event.volunteer_shortage=event.volunteer_req-event.approved_count
     event.put()
     return volunteerShow(request,npoid)
