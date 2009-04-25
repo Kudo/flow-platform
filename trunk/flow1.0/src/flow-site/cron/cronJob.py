793e1656875a28092a9981694fa32c4794977dd0
camgelo <camgelo@56b9cbb4-a45a-11dd-a8c6-677c70d10fe4>
2009-04-25 18:41:10 +0000

    refactoring cron job

--- trunk/flow1.0/src/flow-site/cron/cronJob.py
+++ trunk/flow1.0/src/flow-site/cron/cronJob.py
@@ -9,27 +9,32 @@ def updateEventStatus():
     now=datetime.datetime.utcnow()
     results = db.GqlQuery("SELECT * FROM EventProfile WHERE status = :1 ",'approved').fetch(1000)
     for event in results:
-        if now > event.reg_start_time:
+        if now >= event.reg_start_time:
             event.status='registrating'
             event.put()
 
     results = db.GqlQuery("SELECT * FROM EventProfile WHERE status = :1 ",'registrating').fetch(1000)
     for event in results:
-        if now > event.reg_end_time:
+        if now >= event.reg_end_time:
             event.status='registration closed'
             event.put()
 
     results = db.GqlQuery("SELECT * FROM EventProfile WHERE status = :1 ",'registration closed').fetch(1000)
     for event in results:
-        if now > event.start_time:
+        if now >= event.start_time:
             event.status='on-going'
             event.put()
 
     results = db.GqlQuery("SELECT * FROM EventProfile WHERE status = :1 ",'on-going').fetch(1000)
     for event in results:
-        if now > event.end_time:
+        if now >= event.end_time:
             event.status='activity closed'
             event.put()
+            try:
+                event.npo_profile_ref.total_events+=1
+                event.npo_profile_ref.put()
+            except:
+                logging.error('Update event.npo_profile_ref.total_events failed! event_id=%s'%event.event_id)
 
 def every5minutes(request):
     #if request.META['REMOTE_ADDR']=='0.1.0.1':
@@ -41,15 +46,4 @@ def hourly(request):
     return HttpResponse('ok')
 
 def daily(request):
-    #logging.info('daily cron test')
-    '''
-    message = mail.EmailMessage()
-    message.subject=u'若水志工媒合平台'
-    message.sender = 'camgelo@gmail.com'
-    message.to = 'chienchih_lo@trend.com.tw'
-    message.body = u"""測試GAE的cron job! %s\r\n"""%(datetime.datetime.utcnow()+datetime.timedelta(hours=8))
-    for event in ddl.EventProfile.gql('ORDER BY create_time DESC').fetch(100):
-        message.body+='%s %s %s %s\r\n'%(event.event_name, event.npo_profile_ref.npo_name, event.create_time.strftime('%Y-%m-%d %H:%M'), event.description)
-    message.send()
-    '''
     return HttpResponse('ok')
